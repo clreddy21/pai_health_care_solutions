8e01a5f7b7c1cae9c6d67b9e598ebd500d2acf8d
C Lokesh Reddy <lokesh@lessaccounting.com>
2018-09-17 12:22:52 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,9 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  
+  #setting default headers
+  config.action_dispatch.default_headers = { 'X-Frame-Options' => 'ALLOWALL' }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
